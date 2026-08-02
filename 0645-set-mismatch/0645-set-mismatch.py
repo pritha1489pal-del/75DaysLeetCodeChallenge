f26636ee1ac3e3934453e1f0cff77dfcf228526b
Pritha Pal <pritha1489pal@gmail.com>
2026-08-02 05:59:51 +0530

    Time: 15 ms (62.67%), Space: 14.1 MB (24.1%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        seen = set()
+        duplicate = 0
+        missing = 0
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                missing = i
+                break
+
+        return [duplicate, missing]
