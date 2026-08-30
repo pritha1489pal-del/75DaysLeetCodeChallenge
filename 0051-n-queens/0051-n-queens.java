cc9de0d79a03eddc402e582df417d17457c3edae
Pritha Pal <pritha1489pal@gmail.com>
2026-08-30 11:41:37 +0530

    Time: 1 ms (99.85%), Space: 46.7 MB (46.74%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    List<List<String>> result = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[2 * n - 1];
+        boolean[] diag2 = new boolean[2 * n - 1];
+        backtrack(board, 0, cols, diag1, diag2);
+        return result;
+    }
+    private void backtrack(
+        char[][] board,
+        int row,
+        boolean[] cols,
+        boolean[] diag1,
+        boolean[] diag2
+    ) {
+        //queens placed
+        if (row == board.length) {
+            List<String> solution = new ArrayList<>();
+            for (char[] r : board) {
+                solution.add(new String(r));
+            }
+            result.add(solution);
+            return;
+        }
+        //every column
+        for (int col = 0; col < board.length; col++) {
+            int d1 = row - col + board.length - 1;
+            int d2 = row + col;
+
+            // Check if position is safe
+            if (cols[col] || diag1[d1] || diag2[d2]) {
+                continue;
+            }
+            //queen
+            board[row][col] = 'Q';
+
+            cols[col] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+            //next row
+            backtrack(board, row + 1, cols, diag1, diag2);
+            board[row][col] = '.';
+            cols[col] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
